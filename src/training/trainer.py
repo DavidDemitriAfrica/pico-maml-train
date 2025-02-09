1258a6b805f02ef15e6a6578c4369a37f4ce0d72
DavidDemitriAfrica <daivd.demitri.africa@gmail.com>
2025-02-09 20:04:31 +0000

    try disable autocast

--- src/training/trainer.py
+++ src/training/trainer.py
@@ -552,31 +552,34 @@ class Trainer:
                 classifier_optimizer = torch.optim.SGD(
                     self.model.classifier.parameters(), lr=inner_lr
                 )
-                with higher.innerloop_ctx(
-                    self.model.classifier,
-                    classifier_optimizer,
-                    copy_initial_weights=True,
-                ) as (fclassifier, diffopt):
-                    for _ in range(inner_steps):
-                        _, support_hidden, _ = self.model(
-                            support_inputs["input_ids"],
-                            attention_mask=support_inputs["attention_mask"],
+                with torch.amp.autocast(enabled=False):
+                    with higher.innerloop_ctx(
+                        self.model.classifier,
+                        classifier_optimizer,
+                        copy_initial_weights=True,
+                    ) as (fclassifier, diffopt):
+                        for _ in range(inner_steps):
+                            _, support_hidden, _ = self.model(
+                                support_inputs["input_ids"],
+                                attention_mask=support_inputs["attention_mask"],
+                                return_hidden=True,
+                            )
+                            support_repr = support_hidden.mean(dim=1)
+                            support_preds = fclassifier(support_repr)
+                            support_loss = F.cross_entropy(
+                                support_preds, support_labels
+                            )
+                            diffopt.step(support_loss)
+
+                        # Evaluate on the query set.
+                        _, query_hidden, _ = self.model(
+                            query_inputs["input_ids"],
+                            attention_mask=query_inputs["attention_mask"],
                             return_hidden=True,
                         )
-                        support_repr = support_hidden.mean(dim=1)
-                        support_preds = fclassifier(support_repr)
-                        support_loss = F.cross_entropy(support_preds, support_labels)
-                        diffopt.step(support_loss)
-
-                    # Evaluate on the query set.
-                    _, query_hidden, _ = self.model(
-                        query_inputs["input_ids"],
-                        attention_mask=query_inputs["attention_mask"],
-                        return_hidden=True,
-                    )
-                    query_repr = query_hidden.mean(dim=1)
-                    query_preds = fclassifier(query_repr)
-                    meta_loss = F.cross_entropy(query_preds, query_labels)
+                        query_repr = query_hidden.mean(dim=1)
+                        query_preds = fclassifier(query_repr)
+                        meta_loss = F.cross_entropy(query_preds, query_labels)
 
                 interval_smlmt_loss += meta_loss.item()
                 interval_smlmt_steps += 1
