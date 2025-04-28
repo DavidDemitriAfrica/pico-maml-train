fc023540f4c2615cc38860b1496b1fa3d97c3fb6
DavidDemitriAfrica <daivd.demitri.africa@gmail.com>
2025-04-28 07:45:10 +0100

    sure

--- src/model/pico.py
+++ src/model/pico.py
@@ -318,7 +318,13 @@ class Attention(nn.Module):
         keys = keys.transpose(1, 2)
         values = values.transpose(1, 2)
 
-        with sdpa_kernel(backends=[SDPBackend.EFFICIENT_ATTENTION, SDPBackend.MATH]):
+        with sdpa_kernel(
+            backends=[
+                SDPBackend.FLASH_ATTENTION,
+                SDPBackend.EFFICIENT_ATTENTION,
+                SDPBackend.MATH,
+            ]
+        ):
             attn_output = F.scaled_dot_product_attention(
                 queries.contiguous(),
                 keys.contiguous(),
