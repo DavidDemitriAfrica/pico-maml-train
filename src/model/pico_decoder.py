21c37f492998c68b1a8066727b4296d68a221544
DavidDemitriAfrica <daivd.demitri.africa@gmail.com>
2025-05-04 14:15:51 +0800

    reconstruct embeddings w proper names

--- src/model/pico_decoder.py
+++ src/model/pico_decoder.py
@@ -535,16 +535,16 @@ class PicoDecoder(nn.Module):
         """
         Expand the token_embedding (and tied lm_head if present) to accommodate new special tokens.
         """
-        old_emb = self.token_embedding
+        old_emb = self.embedding_proj
         old_vocab, dim = old_emb.weight.shape
         new_emb = nn.Embedding(new_vocab_size, dim)
         new_emb.weight.data[:old_vocab].copy_(old_emb.weight.data)
-        self.token_embedding = new_emb
+        self.embedding_proj = new_emb
 
-        # if you tie input & output embeddings via an lm_head:
-        if hasattr(self, "lm_head"):
-            self.lm_head = nn.Linear(dim, new_vocab_size, bias=False)
-            self.lm_head.weight = self.token_embedding.weight
+        old_out = self.de_embedding_proj
+        new_out = nn.Linear(dim, new_vocab_size, bias=False)
+        new_out.weight.data[:old_vocab].copy_(old_out.weight.data)
+        self.de_embedding_proj = new_out
 
 
 ########################################################
